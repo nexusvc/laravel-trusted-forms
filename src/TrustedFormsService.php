834a95eef87e2f9685bd338457a491719f1d8e82
Dan Alvidrez <dan@bayareawebpro.com>
2020-04-23 19:22:38 -0700

    wip

--- src/TrustedFormsService.php
+++ src/TrustedFormsService.php
@@ -2,85 +2,36 @@
 
 namespace BayAreaWebPro\TrustedForms;
 
-use DOMDocument;
-use Illuminate\Pipeline\Pipeline;
+use Illuminate\Support\Facades\Http;
 
 class TrustedFormsService
 {
     /**
-     * The pipeline instance.
-     * @var Pipeline
-     */
-    protected Pipeline $pipeline;
-
-    /**
-     * The array of pipes to be processed.
+     * The array of data to be certified.
      * @var array
      */
-    protected array $pipes;
+    protected array $data;
 
     /**
-     * Make Pipeline instance & process it.
-     * @param string|null $html
-     * @param array $pipes
+     * Make TrustedFormsService & generate certificate.
+     * @param array $data
      * @return mixed
      */
-    public static function make(?string $html = null, array $pipes = [])
+    public static function make(array $data = [])
     {
-        return app(static::class, compact('pipes'))->process($html);
+        return app(static::class, compact('data'))->generate();
     }
 
     /**
      * TrustedFormsService constructor.
-     * @param Pipeline $pipeline
-     * @param array $pipes
+     * @param array $data
      */
-    public function __construct(Pipeline $pipeline, array $pipes = [])
+    public function __construct(array $data = [])
     {
-        $this->pipeline = $pipeline;
-        $this->pipes = $pipes;
+        $this->data = $data;
     }
 
-    /**
-     * Process Pipes
-     * @param string|null $html
-     * @return string|null
-     */
-    protected function process(?string $html = null)
-    {
-        if (empty($html)) return $html;
+    public function generate(){
 
-        return (string)$this->pipeline
-            ->send($this->getDom($html))
-            ->through($this->pipes)
-            ->via('handle')
-            ->then(function (DOMDocument $dom) {
-                return $this->getHtml($dom);
-            });
-    }
-
-    /**
-     * Get Html Content
-     * @param DOMDocument $dom
-     * @return string
-     */
-    protected function getHtml(DOMDocument $dom): string
-    {
-        $html = $dom->saveHTML($dom->getElementsByTagName('body')->item(0));
-        return str_replace(['<body>', '</body>'], '', $html);
-    }
-
-    /**
-     * Get DOMDocument Instance with specified HTML content.
-     * @param string $html
-     * @return DOMDocument
-     */
-    protected static function getDom(string $html): DOMDocument
-    {
-        $doc = new DOMDocument;
-        libxml_use_internal_errors(true);
-        $doc->loadHtml(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
-        libxml_clear_errors();
-        return $doc;
     }
 }
